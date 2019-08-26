794007bb6ad253a0bf46e84f2cd0b024aeef4127
bcwahoo <bcwahoo@gmail.com>
2019-08-26 00:05:35 -0400

    JavaScript Update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,34 @@
-var library = ["touchdown", "interception", "fumble", "sack", "referee", "safety", "endzone", ];
+document.onkeyup = function (event) {
+
+  var userGuess = event.key;
+  var wins;
+  var losses;
+  var library = [
+    "touchdown",
+    "interception",
+    "fumble",
+    "sack",
+    "referee",
+    "safety",
+    "endzone"
+  ];
+  var randomLib = library[Math.floor(Math.random() * library.length)];
+  var largeLib = randomLib.toUpperCase();
+  var magicWord = document.getElementById("magicWord");
+  var answerArray = [];
+  for (var i = 0; largeLib.length; i++) {
+    answerArray[i] = "_";
+  }
+  var remainingLetters = largeLib.length;
+
+  while (remainingLetters > 0) {
+    magicWord.innerHTML = answerArray.join(" ");
+
+    for (var j = 0; j < largeLib.length; j++) {
+      if (largeLib[j] === userGuess) {
+        answerArray[j] = userGuess.toUpperCase;
+        remainingLetters--;
+      }
+    }
+  };
+};
