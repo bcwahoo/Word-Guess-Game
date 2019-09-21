211f09448f5fb6e4ec8eaac063ee4ba6980d1fb6
bcwahoo <bcwahoo@gmail.com>
2019-09-21 10:25:01 -0400

    Saturday Upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,40 @@
-document.onkeyup = function (event) {
+var wins;
+var losses;
+var library = [
+  "touchdown",
+  "interception",
+  "fumble",
+  "sack",
+  "referee",
+  "safety",
+  "endzone"
+];
 
+document.onkeyup = function(event) {
   var userGuess = event.key;
-  var wins;
-  var losses;
-  var library = [
-    "touchdown",
-    "interception",
-    "fumble",
-    "sack",
-    "referee",
-    "safety",
-    "endzone"
-  ];
+  var largeGuess = userGuess.toUpperCase();
+  var badWord = document.getElementById("badWord");
   var randomLib = library[Math.floor(Math.random() * library.length)];
   var largeLib = randomLib.toUpperCase();
   var magicWord = document.getElementById("magicWord");
   var answerArray = [];
-  for (var i = 0; largeLib.length; i++) {
-    answerArray[i] = "_";
+  var badArray = [];
+  for (var i = 0; i < largeLib.length; i++) {
+    answerArray[i] = "-";
   }
   var remainingLetters = largeLib.length;
-
   while (remainingLetters > 0) {
     magicWord.innerHTML = answerArray.join(" ");
+    remainingLetters--;
+  }
 
-    for (var j = 0; j < largeLib.length; j++) {
-      if (largeLib[j] === userGuess) {
-        answerArray[j] = userGuess.toUpperCase;
-        remainingLetters--;
-      }
+  for (var j = 0; j < largeLib.length; j++) {
+    if (largeLib[j] === userGuess) {
+      answerArray[j] = userGuess;
+    } else {
+      badArray[i] = badArray.push(largeGuess);
     }
-  };
+    // console.log("hi");
+    // badWord.textContent = largeGuess;
+  }
 };
